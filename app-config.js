6df8c1478b3b60b78475b28710c06c71bfc71fa7
baadishy <minasamirfarag@yahoo.com>
2026-02-26 14:56:39 +0200

    Update app-config locally

--- app-config.js
+++ app-config.js
@@ -1,2 +1,2 @@
-// window.APP_CONFIG = { API_BASE: "http://localhost:3000/api" };
-window.APP_CONFIG = { API_BASE: "https://math-falta.vercel.app/api" };
+window.APP_CONFIG = { API_BASE: "http://localhost:3000/api" };
+// window.APP_CONFIG = { API_BASE: "https://math-falta.vercel.app/api" };
